c001c4839e7668cfca73efd6eab8ae522554428b
sivaram000 <katta.sivaram@suprsend.com>
2023-09-30 18:14:22 +0530

    added types

---
+++ src/index.d.ts
@@ -0,0 +1,58 @@
+interface Dictionary {
+  [key: string]: any
+}
+
+interface IToastOptions {
+  duration?: number
+}
+
+interface IToastProps {
+  position?: string
+  toastOptions?: IToastOptions
+  toastComponent?: ({
+    notificationData
+  }: {
+    notificationData: any
+  }) => JSX.Element
+}
+
+interface ISuprSendProviderProps {
+  workspaceKey: string
+  distinctId: string | null
+  subscriberId: string | null
+  themeType?: 'light' | 'dark'
+  hideAvatar?: boolean
+  hideInbox?: boolean
+  hideToast?: boolean
+  collapseToastNotifications?: boolean
+  noNotificationsComponent?: () => JSX.Element
+  notificationComponent?: ({
+    notificationData
+  }: {
+    notificationData: any
+  }) => JSX.Element
+  bellComponent?: () => JSX.Element
+  badgeComponent?: ({ count }: { count: number | null }) => JSX.Element
+  notificationClickHandler?: (notificationData: any) => void
+  toastProps?: IToastProps
+  theme?: Dictionary
+}
+
+declare function SuprSendInbox({
+  distinctId,
+  subscriberId,
+  workspaceKey,
+  themeType,
+  hideAvatar,
+  hideInbox,
+  hideToast,
+  collapseToastNotifications,
+  noNotificationsComponent,
+  notificationComponent,
+  bellComponent,
+  badgeComponent,
+  notificationClickHandler,
+  toastProps,
+  theme
+}: ISuprSendProviderProps): JSX.Element
+export default SuprSendInbox
