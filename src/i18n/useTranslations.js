add6d70c9d6bac9d7f2753017a4e98d45b629d59
Charles Killer <charles.killer@apolitical.co>
2025-04-07 15:48:16 +0100

    allow translations to be dynamically provided

--- src/i18n/useTranslations.js
+++ src/i18n/useTranslations.js
@@ -1,17 +1,28 @@
-import { useCallback } from 'react'
-import translations from './Languages'
+import { useCallback, useContext, useMemo } from 'react'
+import defaultTranslations from './Languages'
+import { InboxContext } from '../utils/context'
+import { mergeDeep } from '../utils'
 
 const DEFAULT_LANG = 'en'
 
 export const useTranslation = (language = DEFAULT_LANG) => {
+  const { translations: propTranslations } = useContext(InboxContext)
+
+  const allTranslations = useMemo(() => {
+    if (!propTranslations || Object.keys(propTranslations).length === 0) {
+      return defaultTranslations
+    }
+    return mergeDeep(defaultTranslations, propTranslations)
+  }, [propTranslations])
+
   const t = useCallback(
     (key) => {
       return (
-        translations[language]?.translations?.[key] ||
-        translations[DEFAULT_LANG]?.translations?.[key]
+        allTranslations[language]?.translations?.[key] ||
+        allTranslations[DEFAULT_LANG]?.translations?.[key]
       )
     },
-    [language]
+    [language, allTranslations]
   )
 
   return { t, language }
